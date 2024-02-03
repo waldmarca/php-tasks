6edb32ed48503b9fbc517b614dd131a73f9e7800
Sergey Cherniy <antkrid@gmail.com>
2024-02-04 02:27:55 +0300

    HW 3.1

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php 
 include('./src/functions.php');
 
-echo task1([1, 2, 3], false);
+for ($i = 1; $i <= 50; $i++) {
+  $users[] = createUser($i);
+}
 
-echo task2('/', 21, 3, 7);
+saveUsers($users);
 
-echo task3(4, 7);
+calcUsers();
 
-echo task4();
-
-echo task5();
-
-file_put_contents('test.txt', 'Hello again!');
-
-my_file_get_contents('test.txt');
+calcAge();
+?>
--- src/functions.php
+++ src/functions.php
@@ -1,101 +1,41 @@
 <?php
-
-function task1(array $strings, bool $return = true) 
+function createUser($id) 
 {
-  $result = implode("\n", array_map(function (string $str) {
-    return "<p>$str</p>";
-  }, $strings));
+  $names = ['Sergey', 'Maria', 'Ksenia', 'Trevor', 'Katya'];
+  
+  $user = [
+    'id' => $id,
+    'name' => $names[array_rand($names)],
+    'age' => mt_rand(18, 45),
+  ];
 
-  if ($return) {
-    return $result;
-  }
-
-  echo $result;
+  return $user;
 }
 
-function task2(string $action, ...$args) 
+function saveUsers($users) 
 {
-  foreach ($args as $n => $arg) {
-    if (is_int($arg) && !is_float($arg)) {
-      trigger_error('argument #' . $n . 'is not integer or float');
-      return 'ERROR: wrong argument';
-    }
-  }
-  switch ($action) {
-    case '+':
-      return array_sum($args);
-    case '-':
-      return array_shift($args) - array_sum($args);
-    case '/':
-      $result = array_shift($args);
-      foreach ($args as $n => $arg) {
-        if ($arg == 0) {
-          trigger_error('divide by zero on argument #' . ($n + 1));
-          return 'ERROR: divide by zero';
-        }
-        $result = $result / $arg;
-      }
-      return $result;
-    case '*':
-      $result = 1;
-      foreach ($args as $arg) {
-        $result *= $arg;
-      }
-      return $result;
-    
-    default:
-      return 'ERROR: unexpected action';
-  }
+  file_put_contents('./users.json', json_encode($users));
 }
 
-function task3(int $a, int $b)
+function getUsers() 
 {
-    $result = '<table border="1">';
-    for ($i = 1; $i <= $a; $i++) {
-        $result .= '<tr>';
-        for ($j = 1; $j <= $b; $j++) {
-            $result .= '<td>';
-            $result .= $i * $j;
-            $result .= '</td>';
-        }
-        $result .= '</tr>';
-    }
-    $result .= '</table>';
-    echo $result;
+  $data = file_get_contents('./users.json');
+  $decodedUsers = json_decode($data, true);
+
+  return $decodedUsers;
 }
 
-function task4()
+function calcUsers() 
 {
-  date_default_timezone_set('Europe/Moscow');
-  echo date('d.m.Y H:i');
-  echo '<br>';
-  echo strtotime('24.02.2016 00:00:00');
-  echo '<br>';
-  echo date('Y-m-d H:i:s', 1456261200);
-  echo '<br>';
+  $name = array_column(getUsers(), 'name');
+  $listName = array_count_values($name);
+  echo '<pre>';
+  print_r($listName);
 }
 
-function task5() {
-  $string = 'Карл у Клары украл Кораллы';
-  echo str_replace('К', '', $string);
-  echo '<br>';
-
-  $string = 'Две бутылки лимонада';
-  echo str_replace('Две', 'Три', $string);
-  echo '<br>';
-}
-
-function my_file_get_contents(string $filename) 
+function calcAge() 
 {
-  $fp = fopen($filename, 'r');
-  if (!$fp) {
-    return false;
-  }
-
-  $str = '';
-  while (!feof($fp)) {
-    $str .= fgets($fp, 1024);
-  }
-
-  echo $str;
+  $sumAge = array_sum(array_column(getUsers(), 'age'));
+  echo "Средний возраст: " . ($sumAge / sizeof(getUsers()));
 }
+?>
